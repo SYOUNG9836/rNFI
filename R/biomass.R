4d5de137df928282b93299d447f81d21bfe80c9d
@SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2021-07-28 17:52:38 +0900

    up

--- R/biomass.R
+++ R/biomass.R
@@ -195,7 +195,7 @@ biomass <- function(data, byplot= FALSE, grpby=NULL){
       data <- data %>% filter(data$'대경목조사원내존재여부' == 0 )
       plot_num <- n_distinct(data$'표본점번호')
       bm <- data %>% 
-        group_by(grpby) %>%  
+        group_by(data[,grpby] %>%  
         summarise(bm_volume = sum(get('추정간재적'), na.rm=TRUE),
                          bm_biomass = sum(.data$T_biomass, na.rm=TRUE),
                          bm_AG = sum(.data$AG_biomass, na.rm=TRUE),
@@ -207,7 +207,7 @@ biomass <- function(data, byplot= FALSE, grpby=NULL){
                   bm_carbon_ha = sum(.data$carbon_stock, na.rm=TRUE)/(0.04*plot_num),
                   bm_co2_ha = sum(.data$co2_stock, na.rm=TRUE)/(0.04*plot_num),.groups = 'drop')
       
-      bm <- bm %>% rename("volume_m3"= "bm_volume", "biomass_ton" = "bm_biomass", "AG_biomasS_ton" = "bm_AG" ,
+      bm <- bm %>% rename("grpby"= "data[, grpby]","volume_m3"= "bm_volume", "biomass_ton" = "bm_biomass", "AG_biomasS_ton" = "bm_AG" ,
                           "carbon_stock_tC" = "bm_carbon", "co2_stock_tCO2" = "bm_co2", 
                           "volume_m3_ha"= "bm_volume_ha", "biomass_ton_ha" = "bm_biomass_ha", "AG_biomass_ton_ha" = "bm_AG_ha" ,
                           "carbon_stock_tC_ha" = "bm_carbon_ha", "co2_stock_tCO2_ha" = "bm_co2_ha")
