b29221cb9440c2ebbda3e5caac365c7ae638a09b
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2022-05-17 09:56:16 +0900

    new

--- R/index.R
+++ R/index.R
@@ -94,13 +94,13 @@ summary_NFI<- function(data, grpby="표본점번호"){
   
   data_temp <- data_temp %>%
     group_by(data_temp[,grpby]) %>%
-    summarise(count = n(), mean_DBH = mean(get('흉고직경'), na.rm=TRUE), 
+    summarise(num_tree = n(), num_species= n_distinct(get('수종명')),  mean_DBH = mean(get('흉고직경'), na.rm=TRUE), 
               mean_H = mean(get('추정수고'), na.rm=TRUE),
               mean_dominant_H = mean(get('추정수고')[get('수관급')=="우세목"], na.rm=TRUE),
               mean_basal= mean(basal, na.rm=TRUE),
               mean_volume= mean(get('추정간재적'), na.rm=TRUE),.groups = 'drop')
   
-  data_temp <- data_temp %>% rename("grpby"= "data_temp[,grpby]")
+  #data_temp <- data_temp %>% rename("grpby"= "data_temp[,grpby]")
   
   return(data_temp)
   
