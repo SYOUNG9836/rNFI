e5d5ef27e7c8a23de673722a06185f9275c088f7
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2022-06-27 15:13:21 +0900

    new

--- R/flowNFI.R
+++ R/flowNFI.R
@@ -10,9 +10,10 @@
 
 flowNFI <- function(data, grpby="", type = "biomass"){
   
+  flow <- vector("list", length = (length(unique(data$"조사연도"))-4))
+  
   for(i in 1:(length(unique(data$"조사연도"))-4)){ 
-    
-    flow <- vector("list", length = (length(unique(data$"조사연도"))-4))
+  
     s_year <- min(data$"조사연도")+i-1
     e_year <- min(data$"조사연도")+i-1+4
     data_temp <- data %>% filter(data$"조사연도" >= s_year & data$"조사연도" <= e_year)
@@ -23,12 +24,13 @@ flowNFI <- function(data, grpby="", type = "biomass"){
       flow[[i]]$year <- e_year
     }
     else if(type == "iv"){
-      flow[[i]] <- importancevalue_data(data_temp)
+      flow[[i]] <- importancevalue_NFI(data_temp)
       flow[[i]]$year <- e_year
     }
   }
   
-  flow_df <- do.call(rbind, flow)
+  flow_df <- data.table::rbindlist(flow, fill=TRUE, use.names=TRUE)
+  flow_df <- as.data.frame(flow_df)
   
 }
   
