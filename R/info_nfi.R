e4ac57e0ac8f94d932ac72d4d4ad7a902e73135d
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2024-05-17 11:57:47 +0900

    commit

--- R/info_nfi.R
+++ R/info_nfi.R
@@ -9,9 +9,9 @@
 NULL
 
 
-# .onLoad <- function(libname, pkgname) {
-#   repos = getOption("repos")
-#   repos["kadmin"] = "https://SYOUNG9836.github.io/drat/"
-#   options(repos = repos)
-#   invisible(repos)
-# }
+.onLoad <- function(libname, pkgname) {
+  repos = getOption("repos")
+  repos["kadmin"] = "https://SYOUNG9836.github.io/drat/"
+  options(repos = repos)
+  invisible(repos)
+}
