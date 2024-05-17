ee5b25b7b53c81cd6ab71c6ac13e42000c931672
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2024-05-17 12:27:59 +0900

    commit

--- R/tsvis_NFI.R
+++ R/tsvis_NFI.R
@@ -68,10 +68,11 @@ tsvis_NFI <- function(data, grpby=NULL, y=NULL, type = "biomass", output ="line"
       if (consent == 1) {
         drat::addRepo("SYOUNG9836")
         install.packages("kadmin")
+        library(kadmin)
       } else {
         stop("Package 'kadmin' is not installed. This functionality cannot be used without installing the required package.")
       }
-    }
+    }else(library(kadmin))
   }
   
   
@@ -125,17 +126,20 @@ tsvis_NFI <- function(data, grpby=NULL, y=NULL, type = "biomass", output ="line"
                                     FUN=function(x){district_code$district_name[which(x==district_code$district_CD)]}))
       
       if(nchar(tsvis_df[, grpby][1]) == 10){
-        emd <- kadmin::emd
+        data(emd)
+        emd <- force(emd)
         bm_map <- right_join(emd, tsvis_df, by=c("EMD_CD" = quo_name(grpby)))
         #bm_map <- sf::st_as_sf( bm_map )
         
       }else if(nchar(tsvis_df[,grpby][1]) == 5){
-        sgg <- kadmin::sgg
+        data(sgg)
+        sgg <- force(sgg)
         bm_map <- right_join(sgg, tsvis_df, by=c("SIG_CD" = quo_name(grpby)))
         #bm_map <- sf::st_as_sf( bm_map )
         
       }else{
-        do <- kadmin::do
+        data(do)
+        do <- force(do)
         bm_map <- right_join(do, tsvis_df, by=c("CTPRVN_CD" = quo_name(grpby)))
         #bm_map <- sf::st_as_sf( bm_map )
       }
