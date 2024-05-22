70e34e6111f70b5083dd35b4fea174b535a99aa0
SYOUNG9836 <youngsin0306@kookmin.ac.kr>
2024-05-22 17:50:01 +0900

    commit

--- R/info_nfi.R
+++ R/info_nfi.R
@@ -23,41 +23,41 @@
 #' @importFrom tidyr spread
 #' 
 NULL
-
-.pkgenv <- new.env(parent = emptyenv())
-
-.onLoad <- function(libname, pkgname) {
-  has_data <- requireNamespace("kadmin", quietly = TRUE)
-  .pkgenv[["kadmin"]] <- has_data
-  # repos = getOption("repos")
-  # repos["kadmin"] = "https://SYOUNG9836.github.io/drat/"
-  # options(repos = repos)
-  # invisible(repos)
-}
-
-
-.onAttach <- function(libname, pkgname) {
-  if (!.pkgenv$has_data) {
-    msg <- paste("To access larger datasets in this package, install the",
-                 "kadmin package with:\n",
-                 "`install.packages('kadmin',", 
-                 "repos='https://SYOUNG9836.github.io/drat/', type='source')`")
-    msg <- paste(strwrap(msg), collapse="\n")
-    packageStartupMessage(msg)
-  }
-  
-  if(interactive() || getOption("verbose"))
-    packageStartupMessage(sprintf("Package %s (%s) loaded. Check out our website at https://github.com/SYOUNG9836/rNFI.\nType citation(\"%s\") for examples of how to cite rFIA.\n", pkgname,
-                                  packageDescription(pkgname)$Version, pkgname))
-}
-
-hasData <- function(has_data = .pkgenv$has_data) {
-  if (!has_data) {
-    msg <- paste("To use this function, you must have the",
-                 "`kadmin` package installed.")
-    msg <- paste(strwrap(msg), collapse="\n")
-    stop(msg)
-  }
-}
-
-
+# 
+# .pkgenv <- new.env(parent = emptyenv())
+# 
+# .onLoad <- function(libname, pkgname) {
+#   has_data <- requireNamespace("kadmin", quietly = TRUE)
+#   .pkgenv[["kadmin"]] <- has_data
+#   # repos = getOption("repos")
+#   # repos["kadmin"] = "https://SYOUNG9836.github.io/drat/"
+#   # options(repos = repos)
+#   # invisible(repos)
+# }
+# 
+# 
+# .onAttach <- function(libname, pkgname) {
+#   if (!.pkgenv$has_data) {
+#     msg <- paste("To access larger datasets in this package, install the",
+#                  "kadmin package with:\n",
+#                  "`install.packages('kadmin',", 
+#                  "repos='https://SYOUNG9836.github.io/drat/', type='source')`")
+#     msg <- paste(strwrap(msg), collapse="\n")
+#     packageStartupMessage(msg)
+#   }
+#   
+#   if(interactive() || getOption("verbose"))
+#     packageStartupMessage(sprintf("Package %s (%s) loaded. Check out our website at https://github.com/SYOUNG9836/rNFI.\nType citation(\"%s\") for examples of how to cite rFIA.\n", pkgname,
+#                                   packageDescription(pkgname)$Version, pkgname))
+# }
+# 
+# hasData <- function(has_data = .pkgenv$has_data) {
+#   if (!has_data) {
+#     msg <- paste("To use this function, you must have the",
+#                  "`kadmin` package installed.")
+#     msg <- paste(strwrap(msg), collapse="\n")
+#     stop(msg)
+#   }
+# }
+# 
+# 
